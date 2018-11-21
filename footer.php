5e5efae0d2f1aa69aae4f2730c9ba35b5e92959b
onkarnaik36 <onkar.connect@gmail.com>
2018-11-21 18:24:11 +0530

    Added google analytics code in the footer. Removed footer code from all php files and added footer.php.

--- footer.php
+++ footer.php
@@ -231,5 +231,14 @@
             });
         }, 4000);
     </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129579601-1"></script>
+    <script>
+     window.dataLayer = window.dataLayer || [];
+     function gtag(){dataLayer.push(arguments);}
+     gtag('js', new Date());
+
+     gtag('config', 'UA-129579601-1');
+    </script>
 </body>
 </html>
